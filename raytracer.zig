59de94cff518b54110d483102ded7bf473374bda
Stefan Lienhard <sl@stefanlienhard.ch>
2022-10-03 00:55:12 +0200

    book 1 done (but I should still add multi-threading to speed it up)

--- raytracer.zig
+++ raytracer.zig
@@ -8,8 +8,6 @@ const stb = @cImport({
 var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 const gpa = general_purpose_allocator.allocator();
 
-var pcg = std.rand.Pcg.init(0x853c49e6748fea9b);
-
 const pi = std.math.pi;
 fn deg2Rad(deg: f32) f32 {
     return deg / 180.0 * pi;
@@ -17,12 +15,6 @@ fn deg2Rad(deg: f32) f32 {
     // return std.math.degreesToRadians(f32, deg);
 }
 const infinity = std.math.inf(f32);
-fn randFloat() f32 {
-    return pcg.random().float(f32);
-}
-fn randFloatRange(min: f32, max: f32) f32 {
-    return min + (max - min) * randFloat();
-}
 
 // Abuse __m128 as 3D vector, allows using the usual math operators (mostly).
 const Vec3 = @Vector(3, f32);
@@ -53,6 +45,16 @@ fn nearZero(v: Vec3) bool {
     const threshold = 1.0e-8;
     return @reduce(.And, @fabs(v) < scalar(threshold));
 }
+fn reflect(v: Vec3, n: Vec3) Vec3 {
+    return v - scalar(2.0 * dot(v, n)) * n;
+}
+fn refract(uv: Vec3, n: Vec3, etai_over_etat: f32) Vec3 {
+    const cos_theta = std.math.min(dot(-uv, n), 1.0);
+    const r_out_perp = scalar(etai_over_etat) * (uv + scalar(cos_theta) * n);
+    const r_out_parallel = scalar(-@sqrt(@fabs(1.0 - lengthSquared(r_out_perp)))) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 const zero = scalar(0.0);
 const one = scalar(1.0);
 const unit_x = Vec3{ 1.0, 0.0, 0.0 };
@@ -65,6 +67,81 @@ const white = scalar(1.0);
 const red = unit_x;
 const green = unit_y;
 const blue = unit_z;
+
+const Rand = struct {
+    pcg: std.rand.Pcg,
+
+    fn init(seed: u64) Rand {
+        return .{ .pcg = std.rand.Pcg.init(seed) };
+    }
+
+    fn uniformFloat(self: *Rand) f32 {
+        return self.pcg.random().float(f32);
+    }
+    fn uniformFloatInRange(self: *Rand, min: f32, max: f32) f32 {
+        return min + (max - min) * self.uniformFloat();
+    }
+
+    fn uniformVector(self: *Rand) Vec3 {
+        return .{
+            self.uniformFloat(),
+            self.uniformFloat(),
+            self.uniformFloat(),
+        };
+    }
+    fn uniformVectorInRange(self: *Rand, min: f32, max: f32) Vec3 {
+        return .{
+            self.uniformFloatInRange(min, max),
+            self.uniformFloatInRange(min, max),
+            self.uniformFloatInRange(min, max),
+        };
+    }
+
+    // Archimedes' Hat-Box Theorem
+    fn uniformUnitVector(self: *Rand) Vec3 {
+        const w_z = 2.0 * self.uniformFloat() - 1.0;
+        const r = @sqrt(1.0 - w_z * w_z);
+        const theta = 2.0 * pi * self.uniformFloat();
+        const w_x = r * @cos(theta);
+        const w_y = r * @sin(theta);
+        return .{ w_x, w_y, w_z };
+    }
+
+    fn uniformDisk(self: *Rand) Vec3 {
+        const theta = 2.0 * pi * self.uniformFloat();
+        const r = @sqrt(self.uniformFloat());
+        return .{ r * @cos(theta), r * @sin(theta), 0.0 };
+    }
+    fn uniformSphere(self: *Rand) Vec3 {
+        // Is it correct to use the cubic root of a uniform random number as radius?
+        // Probably: https://stackoverflow.com/a/5408843
+        const rand_unit = self.uniformUnitVector();
+        const radius = std.math.pow(f32, self.uniformFloat(), 1.0 / 3.0);
+        return scalar(radius) * rand_unit;
+    }
+    fn uniformSphereRejectionSampling(self: *Rand) Vec3 {
+        return while (true) {
+            const p = Vec3{
+                self.uniformFloatInRange(-1.0, 1.0),
+                self.uniformFloatInRange(-1.0, 1.0),
+                self.uniformFloatInRange(-1.0, 1.0),
+            };
+            if (lengthSquared(p) < 1.0) {
+                break p;
+            }
+        } else unreachable;
+    }
+
+    // normal needs to be normalized
+    fn cosineWeightedHemisphere(self: *Rand, normal: Vec3) Vec3 {
+        std.debug.assert(@fabs(length(normal) - 1.0) < 1.0e-5);
+        // This is cosine weighted importance sampling of the direction.
+        // (See: https://twitter.com/mmalex/status/1550765798263758848)
+        return normal + self.uniformUnitVector();
+    }
+};
+var rand = Rand.init(0x853c49e6748fea9b);
+//var pcg = std.rand.Pcg.init(0x853c49e6748fea9b);
 
 const RGB8 = struct {
     r: u8,
@@ -133,7 +210,11 @@ const Sphere = struct {
         rec.t = root;
         rec.pos = r.at(rec.t);
         rec.material = self.material;
-        const outward_normal = (rec.pos - self.center) / scalar(self.radius);
+        // Normalizing by dividing by radius is less precise than re-normalizing.
+        //const outward_normal = (rec.pos - self.center) / scalar(self.radius);
+        //std.debug.assert(@fabs(length(outward_normal) - 1.0) < 1.0e-2);
+        const outward_normal = normalize(rec.pos - self.center);
+        std.debug.assert(@fabs(length(outward_normal) - 1.0) < 1.0e-5);
         rec.setFaceNormal(r, outward_normal);
         return rec;
     }
@@ -183,6 +264,7 @@ const Camera = struct {
     lower_left_corner: Vec3,
     horizontal: Vec3,
     vertical: Vec3,
+    aspect_ratio: f32,
     u: Vec3,
     v: Vec3,
     lens_radius: f32,
@@ -211,9 +293,10 @@ const Camera = struct {
 
         return .{
             .origin = look_from,
+            .lower_left_corner = lower_left_corner,
             .horizontal = horizontal,
             .vertical = vertical,
-            .lower_left_corner = lower_left_corner,
+            .aspect_ratio = aspect_ratio,
             .u = u,
             .v = v,
             .lens_radius = aperture * 0.5,
@@ -221,7 +304,7 @@ const Camera = struct {
     }
 
     pub fn getRay(self: Camera, s: f32, t: f32) Ray {
-        const rd = scalar(self.lens_radius) * randomInUnitDisk();
+        const rd = scalar(self.lens_radius) * rand.uniformDisk();
         const offset = self.u * scalar(rd[0]) + self.v * scalar(rd[1]);
         const pixel_pos = self.lower_left_corner + scalar(s) * self.horizontal + scalar(t) * self.vertical;
         return Ray{
@@ -230,50 +313,6 @@ const Camera = struct {
         };
     }
 };
-
-// Archimedes' Hat-Box Theorem
-fn randomUnitVector() Vec3 {
-    const w_z = 2.0 * randFloat() - 1.0;
-    const r = @sqrt(1.0 - w_z * w_z);
-    const theta = 2.0 * pi * randFloat();
-    const w_x = r * @cos(theta);
-    const w_y = r * @sin(theta);
-    return .{ w_x, w_y, w_z };
-}
-fn randomInUnitSphereRejectionSampling() Vec3 {
-    return while (true) {
-        const p = Vec3{
-            randFloatRange(-1.0, 1.0),
-            randFloatRange(-1.0, 1.0),
-            randFloatRange(-1.0, 1.0),
-        };
-        if (lengthSquared(p) < 1.0) {
-            break p;
-        }
-    } else unreachable;
-}
-// Is it correct to use the cubic root of a uniform random number as radius?
-// Probably: https://stackoverflow.com/a/5408843
-fn randomInUnitSphere() Vec3 {
-    const rand_unit = randomUnitVector();
-    const radius = std.math.pow(f32, randFloat(), 1.0 / 3.0);
-    return scalar(radius) * rand_unit;
-}
-fn randomInUnitDisk() Vec3 {
-    const theta = 2.0 * pi * randFloat();
-    const r = @sqrt(randFloat());
-    return .{ r * @cos(theta), r * @sin(theta), 0.0 };
-}
-
-fn reflect(v: Vec3, n: Vec3) Vec3 {
-    return v - scalar(2.0 * dot(v, n)) * n;
-}
-fn refract(uv: Vec3, n: Vec3, etai_over_etat: f32) Vec3 {
-    const cos_theta = std.math.min(dot(-uv, n), 1.0);
-    const r_out_perp = scalar(etai_over_etat) * (uv + scalar(cos_theta) * n);
-    const r_out_parallel = scalar(-@sqrt(@fabs(1.0 - lengthSquared(r_out_perp)))) * n;
-    return r_out_perp + r_out_parallel;
-}
 
 const Scatter = struct {
     ray_out: Ray,
@@ -284,13 +323,11 @@ const Lambertian = struct {
     albedo: Vec3,
 
     pub fn scatter(self: Lambertian, hit: HitRecord) Scatter {
-        // This is cosine weighted importance sampling of the direction.
-        // (See: https://twitter.com/mmalex/status/1550765798263758848)
         // The pdf is cos(theta) / pi
         // cos cancels the cos in the lighting equation
         // A proper Lambertian BRDF is albeda / pi
         // The pis in the pdf and the BRDF cancel each other.
-        var scatter_dir = hit.normal + randomUnitVector();
+        var scatter_dir = rand.cosineWeightedHemisphere(hit.normal);
         if (nearZero(scatter_dir)) {
             scatter_dir = hit.normal;
         }
@@ -308,9 +345,9 @@ const Metal = struct {
     fuzz: f32,
 
     pub fn scatter(self: Metal, r_in: Ray, hit: HitRecord) ?Scatter {
-        // NOTE: In the book, r_in.dir is normalized. I don't think that is necessary.
-        const reflected = reflect(r_in.dir, hit.normal);
-        const dir_out = reflected + scalar(self.fuzz) * randomInUnitSphere();
+        // NOTE: reflected needs to be normalized because 'fuzz' is assumed to be added to a unit vector.
+        const reflected = reflect(normalize(r_in.dir), hit.normal);
+        const dir_out = reflected + scalar(self.fuzz) * rand.uniformSphere();
 
         // This will absorb the ray if the fuzziness reflects below the surface
         if (dot(dir_out, hit.normal) > 0.0) {
@@ -340,7 +377,7 @@ const Dielectric = struct {
 
         // Read "Reflections and Refractions in Ray Tracing" about TIR etc.
         var out_dir: Vec3 = undefined;
-        if (cannot_refract or reflectanceSchlickApprox(cos_theta, self.refraction_index) > randFloat()) {
+        if (cannot_refract or reflectanceSchlickApprox(cos_theta, self.refraction_index) > rand.uniformFloat()) {
             out_dir = reflect(unit_dir, hit.normal);
         } else {
             out_dir = refract(unit_dir, hit.normal, refraction_ratio);
@@ -398,59 +435,71 @@ fn rayColor(world: *World, r: Ray, depth: isize) Color {
     }
 }
 
-const global_aspect_ratio = 16.0 / 9.0;
-
 pub fn main() !void {
-    const image_width = 400;
-    const image_height = @floatToInt(usize, @as(f32, image_width) / global_aspect_ratio);
-    var image: [image_height][image_width]RGB8 = undefined;
-    const samples_per_pixel = 100;
-    const max_depth = 50;
+    const scenes = [_]fn () World{
+        sceneInitialGrey,
+        sceneFuzzedMetal,
+        sceneDieletricHollow,
+        sceneDieletricHollowNewPerspective,
+        sceneBook1FinalRandom,
+    };
 
-    var world: World = undefined;
-    //world = sceneInitialGrey();
-    //world = sceneFuzzedMetal();
-    //world = sceneDieletricHollow();
-    //world = sceneDieletricHollowNewPerspective();
-    world = sceneDepthOfField();
-    defer world.deinit();
+    for (scenes) |scene_func, scene_idx| {
+        std.debug.print("Render scene: {}\n", .{scene_idx + 1});
 
-    var y: usize = 0;
-    while (y < image_height) : (y += 1) {
-        const remaining_lines = image_height - y;
-        if (remaining_lines % 10 == 0) {
-            std.debug.print("Scanlines remaining: {}\n", .{remaining_lines});
-        }
+        var world = scene_func();
+        defer world.deinit();
 
-        var x: usize = 0;
-        while (x < image_width) : (x += 1) {
-            var pixel_color = zero;
+        const image_width: usize = 1680; // 400
+        const image_height = @floatToInt(usize, @as(f32, image_width) / world.camera.aspect_ratio);
+        const samples_per_pixel = 500; // 100
+        const max_depth = 50;
 
-            var i: usize = 0;
-            while (i < samples_per_pixel) : (i += 1) {
-                const u = (@intToFloat(f32, x) + randFloat()) / @as(f32, image_width - 1);
-                const v = (@intToFloat(f32, y) + randFloat()) / @as(f32, image_height - 1);
-                const ray = world.camera.getRay(u, v);
-                pixel_color += rayColor(&world, ray, max_depth);
+        var image = std.ArrayList(RGB8).init(gpa);
+        defer image.deinit();
+        try image.resize(image_width * image_height);
+
+        var y: usize = 0;
+        while (y < image_height) : (y += 1) {
+            const remaining_lines = image_height - y;
+            if (remaining_lines % 10 == 0) {
+                std.debug.print("Scanlines remaining: {}\n", .{remaining_lines});
             }
 
-            image[y][x] = RGB8.init(pixel_color, samples_per_pixel);
-        }
-    }
+            var x: usize = 0;
+            while (x < image_width) : (x += 1) {
+                var pixel_color = zero;
 
-    stb.stbi_flip_vertically_on_write(1);
-    _ = stb.stbi_write_png(
-        "render.png",
-        image_width,
-        image_height,
-        3,
-        @ptrCast(*const anyopaque, &image),
-        image_width * 3,
-    );
+                var i: usize = 0;
+                while (i < samples_per_pixel) : (i += 1) {
+                    const u = (@intToFloat(f32, x) + rand.uniformFloat()) / @intToFloat(f32, image_width - 1);
+                    const v = (@intToFloat(f32, y) + rand.uniformFloat()) / @intToFloat(f32, image_height - 1);
+                    const ray = world.camera.getRay(u, v);
+                    pixel_color += rayColor(&world, ray, max_depth);
+                }
+
+                image.items[y * image_width + x] = RGB8.init(pixel_color, samples_per_pixel);
+            }
+        }
+
+        var buf: [32]u8 = undefined;
+        const file_name = try std.fmt.bufPrintZ(buf[0..], "render_{d:0>2}.png", .{scene_idx + 1});
+
+        stb.stbi_flip_vertically_on_write(1);
+        _ = stb.stbi_write_png(
+            //"render.png",
+            file_name,
+            @intCast(c_int, image_width),
+            @intCast(c_int, image_height),
+            3,
+            @ptrCast(*const anyopaque, image.items),
+            @intCast(c_int, image_width * 3),
+        );
+    }
 }
 
 fn sceneInitialGrey() World {
-    const cam = Camera.init(zero, -unit_z, unit_y, 90.0, global_aspect_ratio, 0.0, 1.0);
+    const cam = Camera.init(zero, -unit_z, unit_y, 90.0, (16.0 / 9.0), 0.0, 1.0);
     var world = World.init(cam);
 
     const mat_gray = world.addMaterial(.{
@@ -472,7 +521,7 @@ fn sceneInitialGrey() World {
 }
 
 fn sceneFuzzedMetal() World {
-    const cam = Camera.init(zero, -unit_z, unit_y, 90.0, global_aspect_ratio, 0.0, 1.0);
+    const cam = Camera.init(zero, -unit_z, unit_y, 90.0, (16.0 / 9.0), 0.0, 1.0);
     var world = World.init(cam);
 
     const mat_ground = world.addMaterial(.{
@@ -513,7 +562,7 @@ fn sceneFuzzedMetal() World {
 }
 
 fn sceneDieletricHollow() World {
-    const cam = Camera.init(zero, -unit_z, unit_y, 90.0, global_aspect_ratio, 0.0, 1.0);
+    const cam = Camera.init(zero, -unit_z, unit_y, 90.0, (16.0 / 9.0), 0.0, 1.0);
     var world = World.init(cam);
 
     const mat_ground = world.addMaterial(.{
@@ -560,7 +609,7 @@ fn sceneDieletricHollow() World {
 
 fn sceneDieletricHollowNewPerspective() World {
     var v = sceneDieletricHollow();
-    v.camera = Camera.init(.{ -2.0, 2.0, 1.0 }, -unit_z, unit_y, 20.0, global_aspect_ratio, 0.0, 1.0);
+    v.camera = Camera.init(.{ -2.0, 2.0, 1.0 }, -unit_z, unit_y, 20.0, (16.0 / 9.0), 0.0, 1.0);
     return v;
 }
 
@@ -570,6 +619,65 @@ fn sceneDepthOfField() World {
     const to = -unit_z;
     const dist_to_focus = length(to - from);
     const aperture = 2.0;
-    v.camera = Camera.init(from, to, unit_y, 20.0, global_aspect_ratio, aperture, dist_to_focus);
+    v.camera = Camera.init(from, to, unit_y, 20.0, (16.0 / 9.0), aperture, dist_to_focus);
     return v;
+}
+
+fn sceneBook1FinalRandom() World {
+    const cam = Camera.init(.{ 13.0, 2.0, 3.0 }, zero, unit_y, 20.0, (3.0 / 2.0), 0.1, 10.0);
+    var world = World.init(cam);
+
+    const mat_gray = world.addMaterial(.{
+        .lambertian = .{ .albedo = scalar(0.5) },
+    });
+    world.addSphere(.{
+        .center = .{ 0.0, -1000.0, -1.0 },
+        .radius = 1000.0,
+        .material = mat_gray,
+    });
+
+    const mat1 = world.addMaterial(.{ .dielectric = .{ .refraction_index = 1.5 } });
+    world.addSphere(.{ .center = .{ 0.0, 1.0, 0.0 }, .radius = 1.0, .material = mat1 });
+
+    const mat2 = world.addMaterial(.{ .lambertian = .{ .albedo = .{ 0.4, 0.2, 0.1 } } });
+    world.addSphere(.{ .center = .{ -4.0, 1.0, 0.0 }, .radius = 1.0, .material = mat2 });
+
+    const mat3 = world.addMaterial(.{ .metal = .{ .albedo = .{ 0.7, 0.6, 0.5 }, .fuzz = 0.0 } });
+    world.addSphere(.{ .center = .{ 4.0, 1.0, 0.0 }, .radius = 1.0, .material = mat3 });
+
+    const mat_dielectric = mat1;
+    const radius = 0.2;
+
+    var a: isize = -11;
+    while (a < 11) : (a += 1) {
+        var b: isize = -11;
+        while (b < 11) : (b += 1) {
+            const center = Vec3{ @intToFloat(f32, a) + 0.9 * rand.uniformFloat(), 0.2, @intToFloat(f32, b) + 0.9 * rand.uniformFloat() };
+
+            if (length(center - Vec3{ 4, 0.2, 0 }) > 0.9) {
+                const choose_mat = rand.uniformFloat();
+
+                if (choose_mat < 0.8) { // diffuse
+                    const mat = world.addMaterial(.{
+                        .lambertian = .{
+                            .albedo = rand.uniformVector() * rand.uniformVector(),
+                        },
+                    });
+                    world.addSphere(.{ .center = center, .radius = radius, .material = mat });
+                } else if (choose_mat < 0.95) { // metal
+                    const mat = world.addMaterial(.{
+                        .metal = .{
+                            .albedo = rand.uniformVectorInRange(0.5, 1.0),
+                            .fuzz = rand.uniformFloatInRange(0, 0.5),
+                        },
+                    });
+                    world.addSphere(.{ .center = center, .radius = radius, .material = mat });
+                } else { // glass
+                    world.addSphere(.{ .center = center, .radius = radius, .material = mat_dielectric });
+                }
+            }
+        }
+    }
+
+    return world;
 }
